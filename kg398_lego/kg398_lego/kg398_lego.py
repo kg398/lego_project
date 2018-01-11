857f755b6385b8ba5cdf5f4f68f10dff9f765353
Kieran Gilday <kg398@cam.ac.uk>
2018-01-11 13:41:42 +0000

    improved picking
    updated main and lego_moves for experiment, 
    added previous work(BIRL_IROS) that acted as a basis

--- kg398_lego/kg398_lego/kg398_lego.py
+++ kg398_lego/kg398_lego/kg398_lego.py
@@ -44,11 +44,67 @@ def main():
     
     while True:
         task = raw_input("task: ")
-        if task == "lego":
-            for i in range(0,3):
-                lm.grid_pick(c,ser_ee,7+i*4,7,0,0)
-                lm.grid_place(c,ser_ee,11+i*4,7,0,0)
+        if task == "cal":
+            #ipt = raw_input("Continue?")
+            #lm.grid_pick(c,ser_ee,0,1,0,0)
+            #lm.grid_place(c,ser_ee,0,1,0,0)
+            #ipt = raw_input("Continue?")
+            #lm.grid_pick(c,ser_ee,30,1,0,0)
+            #lm.grid_place(c,ser_ee,30,1,0,0)
+            #ipt = raw_input("Continue?")
+            #lm.grid_pick(c,ser_ee,30,13,0,0)
+            #lm.grid_place(c,ser_ee,30,13,0,0)
+            ipt = raw_input("Continue?")
+            lm.grid_pick(c,ser_ee,0,13,0,0)
+            lm.grid_place(c,ser_ee,0,13,0,0)
+            ipt = raw_input("Continue?")
             msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+        if task == "stack":
+            msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+            time.sleep(3)
+            lm.stack_pick(c,ser_ee,0,1,2,0,stack=3)
+            lm.grid_place(c,ser_ee,30,1,2,0)
+        if task == "rot":
+            msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+            ic.socket_send(c,sCMD=300)
+            current_Pose = ic.get_ur_position(c,1)
+            demand_Pose = {"x":current_Pose[0], "y":current_Pose[1], "z":current_Pose[2], "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
+            print "current_Pose: ",demand_Pose
+            demand_Pose = dict(lm.smooth_rotate(c,0,R=20))
+            print "demand_Pose: ",demand_Pose
+            ipt = raw_input("continue?")
+            msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=8)
+#        if task == "lego":
+#            #time.sleep(5)
+#            ipt = raw_input("Open?(y/n)")
+#            if ipt == "y":
+#                ic.super_serial_send(ser_ee,"G",51)
+#            for i in range(0,3):
+#                for j in range(0,4):
+#                    for k in range(0,2):
+#                        lm.grid_pick(c,ser_ee,j*8+3,k*6+1,4,0)
+#                        lm.grid_place(c,ser_ee,j*8+3,k*6+7,4,0)
+#                    msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+#                for j in range(0,4):
+#                    for k in range(0,2):
+#                        lm.grid_pick(c,ser_ee,27-j*8,13-k*6,4,0)
+#                        lm.grid_place(c,ser_ee,27-j*8,7-k*6,4,0)
+#                    msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+        if task == "lego":
+            #time.sleep(5)
+            ipt = raw_input("Open?(y/n)")
+            if ipt == "y":
+                ic.super_serial_send(ser_ee,"G",51)
+            for i in range(0,4):
+                for j in range(0,4):
+                    for k in range(0,2):
+                        lm.grid_pick(c,ser_ee,((j+k)*8+3)%32,k*6+1,12,0)
+                        lm.grid_place(c,ser_ee,((j+k+1)*8+3)%32,k*6+7,12,0)
+                    msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
+                for j in range(0,4):
+                    lm.grid_pick(c,ser_ee,j*8+3,13,12,0)
+                    lm.grid_place(c,ser_ee,((j+1)*8+3)%32,1,12,0)
+                msg = ic.safe_ur_move(c,Pose=dict(wp.home_joints),CMD=2)
         if task == "wp":
             msg = ic.safe_ur_move(c,Pose=dict(wp.grid_0_1_joints),CMD=2)
         if task == "pose":
@@ -57,27 +113,7 @@ def main():
         if task == "joints":
             current_Joints, current_Grip = ic.get_ur_position(c,3)
             print "current joints: ", current_Joints
-        if task == "0":
-            ic.serial_send(ser_ee,"G",48)
-            while True:
-                ipt = ser_ee.readline()
-                print ipt
-                if ipt == "done\r\n":
-                    break
-        if task == "1":
-            ic.serial_send(ser_ee,"G",49)
-            while True:
-                ipt = ser_ee.readline()
-                print ipt
-                if ipt == "done\r\n":
-                    break
-        if task == "2":
-            ic.serial_send(ser_ee,"G",50)
-            while True:
-                ipt = ser_ee.readline()
-                print ipt
-                if ipt == "done\r\n":
-                    break
+
         if task == "i":
             print ser_ee.readline()
             
--- kg398_lego/kg398_lego/lego_moves.py
+++ kg398_lego/kg398_lego/lego_moves.py
@@ -5,6 +5,7 @@ import time
 import random
 import copy
 import math
+import numpy as np
 
 import ur_interface_cmds as ic
 import ur_waypoints as wp
@@ -12,39 +13,64 @@ import ur_waypoints as wp
 def grid_pick(c,ser_ee,x,y,z,r):
     ic.socket_send(c,sCMD=300)
 
-    demand_Joints = dict(grid_pos(c,x,y,z,r))
+    demand_Joints = dict(grid_pos(c,x,y,z+2,r))
     msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
 
-    ic.super_serial_send(ser_ee,"G",51)
+    #ic.super_serial_send(ser_ee,"G",51)
 
-    current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x": current_Pose[0], "y": current_Pose[1], "z": current_Pose[2]-12, "rx": current_Pose[3], "ry": current_Pose[4], "rz": current_Pose[5]}
-    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4,Speed=0.05)
+    demand_Joints = dict(grid_pos(c,x,y,z+0.3,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.5)
+
+    demand_Joints = dict(grid_pos(c,x,y,z,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.1)
 
     ic.super_serial_send(ser_ee,"G",49)
 
-    current_Joints = ic.get_ur_position(c,3)
-    demand_Joints = {"x": current_Joints[0], "y": current_Joints[1], "z": current_Joints[2], "rx": current_Joints[3], "ry": current_Joints[4]+30, "rz": current_Joints[5]}
-    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.05)
+    demand_Pose = dict(smooth_rotate(c,0,R=20))
+    #print "demand_Pose: ",demand_Pose
+    #ipt = raw_input("continue?")
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=8)
 
-    current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x": current_Pose[0], "y": current_Pose[1], "z": current_Pose[2]+14, "rx": current_Pose[3], "ry": current_Pose[4], "rz": current_Pose[5]}
-    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4,Speed=0.001)
+    demand_Joints = dict(grid_pos(c,x,y,z+1,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.5)
+
+def stack_pick(c,ser_ee,x,y,z,r,stack=1):
+    demand_Joints = dict(grid_pos(c,x,y,z+1,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
+
+    ic.socket_send(c,sCMD=299+stack)
+
+    ic.super_serial_send(ser_ee,"G",51)
+
+    demand_Joints = dict(grid_pos(c,x,y,z,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.1)
+
+    ic.super_serial_send(ser_ee,"G",49)
+
+    demand_Pose = dict(smooth_rotate(c,0,R=20))
+    #print "demand_Pose: ",demand_Pose
+    #ipt = raw_input("continue?")
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=8)
+
+    demand_Joints = dict(grid_pos(c,x,y,z+1,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.5)
 
 def grid_place(c,ser_ee,x,y,z,r):
     ic.socket_send(c,sCMD=300)
     
-    demand_Joints = dict(grid_pos(c,x,y,z,r))
+    demand_Joints = dict(grid_pos(c,x,y,z+2,r))
     msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
 
-    current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x": current_Pose[0], "y": current_Pose[1], "z": current_Pose[2]-12, "rx": current_Pose[3], "ry": current_Pose[4], "rz": current_Pose[5]}
-    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4,Speed=0.001)
+    demand_Joints = dict(grid_pos(c,x,y,z+0.3,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.5)
+
+    demand_Joints = dict(grid_pos(c,x,y,z,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2,Speed=0.1)
 
     ic.super_serial_send(ser_ee,"G",51)
 
-    demand_Pose['z'] = demand_Pose['z']+14
-    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4,Speed=0.05)
+    demand_Joints = dict(grid_pos(c,x,y,z+2,r))
+    msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
     return
 
 def grid_pos(c,x,y,z,r):
@@ -54,14 +80,26 @@ def grid_pos(c,x,y,z,r):
     # r:0/90
     nx = 30
     ny = 12
+    nz = 9
 
-    dx = {"x": (wp.grid_30_1['x']-wp.grid_0_1['x'])/nx, "y": (wp.grid_30_1['y']-wp.grid_0_1['y'])/nx, "z": (wp.grid_30_1['z']-wp.grid_0_1['z'])/nx, "rx": 0, "ry": 0, "rz": 0}
-    dy = {"x": (wp.grid_30_13['x']-wp.grid_30_1['x'])/ny, "y": (wp.grid_30_13['y']-wp.grid_30_1['y'])/ny, "z": (wp.grid_30_13['z']-wp.grid_30_1['z'])/ny, "rx": 0, "ry": 0, "rz": 0}
-    dz = {"x": 0, "y": 0, "z": 9.5, "rx": 0, "ry": 0, "rz": 0}
+    # dx = {"x": (wp.grid_30_1['x']-wp.grid_0_1['x'])/nx, "y": (wp.grid_30_1['y']-wp.grid_0_1['y'])/nx, "z": (wp.grid_30_1['z']-wp.grid_0_1['z'])/nx, "rx": 0, "ry": 0, "rz": 0}
+    dy1 = {"x": (wp.grid_0_13['x']-wp.grid_0_1['x'])/ny, "y": (wp.grid_0_13['y']-wp.grid_0_1['y'])/ny, "z": (wp.grid_0_13['z']-wp.grid_0_1['z'])/ny, "rx": 0, "ry": 0, "rz": 0}
+    dy2 = {"x": (wp.grid_30_13['x']-wp.grid_30_1['x'])/ny, "y": (wp.grid_30_13['y']-wp.grid_30_1['y'])/ny, "z": (wp.grid_30_13['z']-wp.grid_30_1['z'])/ny, "rx": 0, "ry": 0, "rz": 0}
+    dz = {"x": (wp.grid_30_1_10['x']-wp.grid_30_1['x'])/nz, "y": (wp.grid_30_1_10['y']-wp.grid_30_1['y'])/nz, "z": (wp.grid_30_1_10['z']-wp.grid_30_1['z'])/nz, "rx": 0, "ry": 0, "rz": 0}
 
-    grid_Pose = {"x": wp.grid_0_1['x'] + x*dx['x'] + (y-1)*dy['x'] + z*dz['x'], 
-                 "y": wp.grid_0_1['y'] + x*dx['y'] + (y-1)*dy['y'] + z*dz['y'], 
-                 "z": wp.grid_0_1['z'] + x*dx['z'] + (y-1)*dy['z'] + z*dz['z']+10, 
+    y1_Pose = {"x": wp.grid_0_1['x'] + (y-1)*dy1['x'] + z*dz['x'], 
+                 "y": wp.grid_0_1['y'] + (y-1)*dy1['y'] + z*dz['y'], 
+                 "z": wp.grid_0_1['z'] + (y-1)*dy1['z'] + z*dz['z'], 
+                 "rx": wp.grid_0_1['rx'], "ry": wp.grid_0_1['ry'], "rz": wp.grid_0_1['rz']}
+
+    y2_Pose = {"x": wp.grid_30_1['x'] + (y-1)*dy2['x'] + z*dz['x'], 
+                 "y": wp.grid_30_1['y'] + (y-1)*dy2['y'] + z*dz['y'], 
+                 "z": wp.grid_30_1['z'] + (y-1)*dy2['z'] + z*dz['z'], 
+                 "rx": wp.grid_30_1['rx'], "ry": wp.grid_0_1['ry'], "rz": wp.grid_0_1['rz']}
+
+    grid_Pose = {"x": ((nx-x)*y1_Pose['x'] + x*y2_Pose['x'])/nx, 
+                 "y": ((nx-x)*y1_Pose['y'] + x*y2_Pose['y'])/nx, 
+                 "z": ((nx-x)*y1_Pose['z'] + x*y2_Pose['z'])/nx, 
                  "rx": wp.grid_0_1['rx'], "ry": wp.grid_0_1['ry'], "rz": wp.grid_0_1['rz']}
 
     print 'pose: ',grid_Pose
@@ -72,3 +110,25 @@ def grid_pos(c,x,y,z,r):
     grid_Joints = {"x": calculated_Joints[0], "y": calculated_Joints[1], "z": calculated_Joints[2], "rx": calculated_Joints[3], "ry": calculated_Joints[4], "rz": calculated_Joints[5]+r}
 
     return grid_Joints
+
+def smooth_rotate(c,r,R=20):
+    current_Pose = ic.get_ur_position(c,1)
+    T1 = math.pi*math.sqrt(current_Pose[3]*current_Pose[3]+current_Pose[4]*current_Pose[4]+current_Pose[5]*current_Pose[5])/180
+    R1 = np.array([current_Pose[3]*math.pi/(180*T1),
+                   current_Pose[4]*math.pi/(180*T1),
+                   current_Pose[5]*math.pi/(180*T1)])
+
+    if r == 0:
+        T2 = math.pi*R/180
+        dx = (wp.grid_30_13['x']-wp.grid_30_1['x'])*math.pi/180
+        dy = (wp.grid_30_13['y']-wp.grid_30_1['y'])*math.pi/180
+        dz = (wp.grid_30_13['z']-wp.grid_30_1['z'])*math.pi/180
+        norm = math.sqrt(dx*dx+dy*dy+dz*dz)
+        R2 = np.array([dx/norm,
+                       dy/norm,
+                       dz/norm])
+
+    T3 = 2*math.acos(math.cos(T1/2)*math.cos(T2/2)-math.sin(T1/2)*math.sin(T2/2)*np.dot(R1,R2))
+    R3 = (math.sin(T2/2)*math.cos(T1/2)*R2+math.cos(T2/2)*math.sin(T1/2)*R1+math.sin(T2/2)*math.sin(T1/2)*np.cross(R2,R2))/math.sin(T3/2)
+        
+    return {"x": current_Pose[0], "y": current_Pose[1], "z": current_Pose[2], "rx": T3*R3[0]*180/math.pi, "ry": T3*R3[1]*180/math.pi, "rz": T3*R3[2]*180/math.pi}
